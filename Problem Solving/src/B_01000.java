d9b48c1dd4429e1a3344b58ef5abecd05a44955d
Emma10003 <5emma10003@gmail.com>
2025-12-25 16:01:54 +0900

    Baekjoon

---
+++ Problem Solving/src/B_01000.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class B_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫 번째 숫자 입력: ");
+        int num1 = sc.nextInt();
+    }
+}
