f72999d57bf61ed6cf579790003d5553a3ca7d29
Emma10003 <5emma10003@gmail.com>
2026-01-10 21:29:14 +0900

    시간복잡도

---
+++ Problem Solving/src/B_24262.java
@@ -0,0 +1,6 @@
+public class B_24262 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
---
+++ Problem Solving/src/B_24265.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class B_24265 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextInt();
+
+        System.out.println((N * (N-1) / 2));
+        System.out.println(2);
+    }
+    
+}
